7179a3fe04544211e9be0c6d2c607b89340e4a81
coleyoung <ighong11@gmail.com>
2026-09-25 20:38:58 +0900

    Deploy site to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/portfolio/" : "/",
+  base: command === "build" ? "/sw-engineer/" : "/",
   plugins: [react()],
 }));
